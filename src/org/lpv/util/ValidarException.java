e91b1a9171755ecd54c7c9b953182e5000b3efd9
Javier Sian <jsian-2026057>
2026-08-21 11:09:39 -0600

    Se agrego la validación

---
+++ src/org/lpv/util/ValidarException.java
@@ -0,0 +1,38 @@
+package org.lpv.util;
+ 
+public class ValidarException extends Exception {
+ 
+    public ValidarException(String mensaje) {
+        super(mensaje);
+    }
+ 
+    public static void validarNoVacio(String valor, String nombreCampo)
+            throws ValidarException {
+        if (valor == null || valor.trim().isEmpty()) {
+            throw new ValidarException(
+                    "El campo " + nombreCampo + "no puede estar vacio");
+        }
+    }
+ 
+    public static void validarCoincidencia(String a, String b, String mensaje)
+            throws ValidarException {
+        if (!a.equals(b)) {
+            throw new ValidarException(mensaje);
+        }
+    }
+ 
+    public static void validarLongitudMinima(String valor, int min, String mensaje)
+            throws ValidarException {
+        if (valor.length() < min) {
+            throw new ValidarException(mensaje);
+        }
+    }
+ 
+    public static void validarNulo(Object obj, String mensaje)
+            throws ValidarException {
+        if (obj == null) {
+            throw new ValidarException(mensaje);
+        }
+ 
+    }
+}
