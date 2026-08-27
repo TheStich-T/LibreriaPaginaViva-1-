f97c33caed1a926f9d3f6cd68be47def7ffce4de
Informatica <informatica@alumno.local>
2026-08-27 11:38:24 -0600

    feat(ft/login) - se creo las clase FXML de login con su controller y su css

---
+++ src/org/lpv/controller/LoginController.java
@@ -0,0 +1,15 @@
+
+package org.lpv.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class LoginController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
