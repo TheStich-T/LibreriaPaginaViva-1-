5e1b642cad08a5847cea84ee902fbb713493736e
Antony Perez <aperez-2023350@kinal.edu.gt>
2026-09-11 09:44:03 -0600

    fead(ProveedorDAO) - se creo la interfaz DAO de Proveedor

--- src/org/lpv/model/Proveedor.java
+++ src/org/lpv/model/Proveedor.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.lpv.model;
 
-/**
- *
- * @author informatica
- */
 public class Proveedor {
-    
+
+    private String nitProveedor;
+    private String nombreProveedor;
+    private String telefonoProveedor;
+    private String direccionProveedor;
+
+    public Proveedor() {
+    }
+
+    public Proveedor(String nitProveedor, String nombreProveedor,
+                      String telefonoProveedor, String direccionProveedor) {
+        this.nitProveedor = nitProveedor;
+        this.nombreProveedor = nombreProveedor;
+        this.telefonoProveedor = telefonoProveedor;
+        this.direccionProveedor = direccionProveedor;
+    }
+
+    public String getNitProveedor() {
+        return nitProveedor;
+    }
+
+    public void setNitProveedor(String nitProveedor) {
+        this.nitProveedor = nitProveedor;
+    }
+
+    public String getNombreProveedor() {
+        return nombreProveedor;
+    }
+
+    public void setNombreProveedor(String nombreProveedor) {
+        this.nombreProveedor = nombreProveedor;
+    }
+
+    public String getTelefonoProveedor() {
+        return telefonoProveedor;
+    }
+
+    public void setTelefonoProveedor(String telefonoProveedor) {
+        this.telefonoProveedor = telefonoProveedor;
+    }
+
+    public String getDireccionProveedor() {
+        return direccionProveedor;
+    }
+
+    public void setDireccionProveedor(String direccionProveedor) {
+        this.direccionProveedor = direccionProveedor;
+    }
+
+    // usado por el ComboBox para mostrar "NIT - Nombre"
+    @Override
+    public String toString() {
+        return nitProveedor + " - " + nombreProveedor;
+    }
 }
